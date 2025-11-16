41e633e00c3e8088cc84f016b5a7d3132ce9d5ba
Marvin_Rop <marwin5411@outlook.com>
2025-11-16 17:09:47 +0530

    Add env_file to PM2 config to load .env.production

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,6 +9,7 @@ module.exports = {
     autorestart: true,
     watch: false,
     max_memory_restart: '500M',
+    env_file: '.env.production',
     env: {
       NODE_ENV: 'production',
       PORT: 3100
